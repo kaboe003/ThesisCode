79e068074c906fd7e3f3d602f6f1dcbf6e9b6e76
kaboe003 <ammagedon@hotmail.de>
2019-11-18 15:43:38 +0100

    Complete Data Structures for Saving into a Database

--- TeethCounterAction.js
+++ TeethCounterAction.js
@@ -1,26 +1,40 @@
 const { withHermes } = require('hermes-javascript')
-
+var zst;
+var mu;
+var zaehne = new Map();
+var info = new Map();
+var piz;
 withHermes(hermes => {
   const dialog = hermes.dialog()
-
+  dialog.flow('PatientIntent', (msg, flow) => {
+    piz = msg.slots[0].value.value;
+    console.log(msg))
+    flow.end()
+    return "Guten Tag Patient" + msg.slots[0].value.value
+  })
   dialog.flow('ZSTIntent', (msg, flow) => {
+    zst = msg.slot[0].value.value;
+    info.set('Zahnstein', zst);
     console.log(msg))
     flow.end()
     return "Verstanden"
   })
   dialog.flow('MUIntent', (msg, flow) => {
+    mu = msg.slot[0].value.value;
+    info.set("Mundkrankheit", mu);
     console.log(msg))
     flow.end()
     return "Verstanden"
   })
   dialog.flow('ZahnIntent', (msg, flow) => {
+    if (zaehne.size() <= 32){
+    zaehne.set('${msg.slots[0].value.value}${msg.slots[1].value.value, msg.slots[2].value.value}')
     console.log(msg))
     flow.end()
     return "Verstanden"
+  }
+  flow.end()
+  return "Es sind bereits alle 32 Zähne erfasst."
   })
-  dialog.flow('PatientIntent', (msg, flow) => {
-    console.log(msg))
-    flow.end()
-    return "Guten Tag Patient" + msg.slots[0].value.value
-  })
+
 })
