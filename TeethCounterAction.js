e0d55f59fcacccd8f230c08079425df95b0ade78
kaboe003 <ammagedon@hotmail.de>
2019-11-18 14:55:53 +0100

    HelloWorld Example (Full)

--- TeethCounterAction.js
+++ TeethCounterAction.js
@@ -3,9 +3,24 @@ const { withHermes } = require('hermes-javascript')
 withHermes(hermes => {
   const dialog = hermes.dialog()
 
-  dialog.flow('ZahnsteinIntent', (msg, flow) => {
+  dialog.flow('ZSTIntent', (msg, flow) => {
     console.log(msg))
     flow.end()
-    return "Testlog"
+    return "Verstanden"
+  })
+  dialog.flow('MUIntent', (msg, flow) => {
+    console.log(msg))
+    flow.end()
+    return "Verstanden"
+  })
+  dialog.flow('ZahnIntent', (msg, flow) => {
+    console.log(msg))
+    flow.end()
+    return "Verstanden"
+  })
+  dialog.flow('PatientIntent', (msg, flow) => {
+    console.log(msg))
+    flow.end()
+    return "Guten Tag Patient" + msg.slots[0].value.value
   })
 })
