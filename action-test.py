62f0ab846dc785649a0b18751b58e1d16b94844e
kaboe003 <ammagedon@hotmail.de>
2020-01-06 12:08:37 +0100

    Update Requirements

--- action-test.py
+++ action-test.py
@@ -6,7 +6,7 @@ import boto3
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
-import io
+
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
